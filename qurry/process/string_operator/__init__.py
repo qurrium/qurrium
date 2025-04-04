0d8ad0d929c7fe3a1cafe3958ab31950ed686817
harui <harui2019@proton.me>
2025-04-05 02:50:02 +0800

    feat(process.string_operator): [Revive] Revive The Qurry Instance of `StringOperator` #269 - post processing

---
+++ qurry/process/string_operator/__init__.py
@@ -0,0 +1,11 @@
+"""
+================================================================
+Postprocessing - String Operator
+(:mod:`qurry.process.string_operator`)
+================================================================
+
+"""
+
+from .strop_core import BACKEND_AVAILABLE as string_operator_availability
+
+from .string_operator import string_operator_order
