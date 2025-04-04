9f529d0e6cb7bda1a3671e8c66ca901088ac72e4
harui <harui2019@proton.me>
2025-04-05 03:05:20 +0800

    feat(qurries.twisted_operator): reproposal

---
+++ qurry/qurries/twisted_operator/__init__.py
@@ -0,0 +1,11 @@
+"""
+===========================================================
+TwistedOperator - Twisted Operator
+(:mod:`qurry.qurries.twisted_operator`)
+===========================================================
+
+- Formerly known as `qurtwistop`
+- Proposal for a new Qurry module
+- Not yet implemented
+
+"""
