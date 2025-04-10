24decd5c57ddf727bce001727ffa26630f66f7ca
harui2019 <harui2019@proton.me>
2025-04-10 18:59:06 +0800

    docs(process.string_operator): add ref

--- qurry/process/string_operator/string_operator.py
+++ qurry/process/string_operator/string_operator.py
@@ -26,6 +26,28 @@ def string_operator_order(
 ) -> StringOperator:
     """Calculate the order of the string operator.
 
+    Reference:
+        .. note::
+            - Crossing a topological phase transition with a quantum computer -
+            Smith, Adam and Jobst, Bernhard and Green, Andrew G. and Pollmann, Frank,
+            [PhysRevResearch.4.L022020](https://link.aps.org/doi/10.1103/PhysRevResearch.4.L022020)
+
+        .. code-block:: bibtex
+            @article{PhysRevResearch.4.L022020,
+                title = {Crossing a topological phase transition with a quantum computer},
+                author = {Smith, Adam and Jobst, Bernhard and Green, Andrew G. and Pollmann, Frank},
+                journal = {Phys. Rev. Research},
+                volume = {4},
+                issue = {2},
+                pages = {L022020},
+                numpages = {8},
+                year = {2022},
+                month = {Apr},
+                publisher = {American Physical Society},
+                doi = {10.1103/PhysRevResearch.4.L022020},
+                url = {https://link.aps.org/doi/10.1103/PhysRevResearch.4.L022020}
+            }
+
     Args:
         shots (int): Number of shots.
         counts (list[dict[str, int]]): List of counts.
