64965698a8e4fac323c20059b24bc641a024ec5b
harui2019 <harui2019@proton.me>
2025-04-25 20:05:28 +0800

    feat(process.magnet_square): Replace `starmap` by `imap` or `imap_unorder` #296

--- qurry/process/magnet_square/magsq_cell.py
+++ qurry/process/magnet_square/magsq_cell.py
@@ -3,14 +3,20 @@
 
 """
 
+import warnings
 from typing import Union
 import numpy as np
 
-from ..availability import availablility, default_postprocessing_backend
+from ..availability import (
+    availablility,
+    default_postprocessing_backend,
+    PostProcessingBackendLabel,
+)
 
-# from ..exceptions import (
-#     PostProcessingRustImportError,
-# )
+from ..exceptions import (
+    # PostProcessingRustImportError,
+    PostProcessingRustUnavailableWarning,
+)
 
 
 # try:
@@ -87,3 +93,51 @@ def magsq_cell_py(
         ratio = np.float64(single_counts[bits]) / shots
         _magnetsq_cell += ratio if bits[0] == bits[1] else -ratio
     return idx, _magnetsq_cell
+
+
+def magsq_cell(
+    idx: int,
+    single_counts: dict[str, int],
+    shots: int,
+    backend: PostProcessingBackendLabel = DEFAULT_PROCESS_BACKEND,
+) -> tuple[int, Union[float, np.float64]]:
+    """Calculate the magnitudes square cell.
+
+    Args:
+        idx (int): Index of the cell (counts).
+        single_counts (dict[str, int]): Single counts of the cell.
+        shots (int): Shots of the experiment on quantum machine.
+        backend (str, optional): Backend to use. Defaults to DEFAULT_PROCESS_BACKEND.
+
+    Returns:
+        tuple[int, Union[float, np.float64]]: Index, one of magnitudes square.
+    """
+
+    if backend == "Rust":
+        warnings.warn(
+            "Rust is not ready, using Python to calculate purity cell.",
+            PostProcessingRustUnavailableWarning,
+        )
+        backend = "Python"
+        # return magsq_cell_rust(idx, single_counts, shots)
+    return magsq_cell_py(idx, single_counts, shots)
+
+
+def magsq_cell_wrapper(
+    arguments: tuple[int, dict[str, int], int, PostProcessingBackendLabel],
+) -> tuple[int, Union[float, np.float64]]:
+    """Wrapper for the magnetic square cell.
+
+    Args:
+        arguments (tuple[int, dict[str, int], int, PostProcessingBackendLabel]):
+            The arguments for the magnetic square cell.
+            - idx (int): Index of the cell (counts).
+            - single_counts (dict[str, int]): Single counts of the cell.
+            - shots (int): Shots of the experiment on quantum machine.
+            - backend (PostProcessingBackendLabel): Backend to use.
+                Defaults to DEFAULT_PROCESS_BACKEND.
+
+    Returns:
+        tuple[int, Union[float, np.float64]]: Index, one of magnitudes square.
+    """
+    return magsq_cell(*arguments)
