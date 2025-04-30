5afc8958041f5c536d39244a1effcb56086a11ef
harui2019 <harui2019@proton.me>
2025-04-30 21:49:08 +0800

    Revert "feat(multimanager): smaller chunksize and maxtaskperchild"
    This reverts commit fe3d2befdf02e3834aa3c6a6dbfc43051fb6db85.

--- qurry/qurrium/multimanager/multimanager.py
+++ qurry/qurrium/multimanager/multimanager.py
@@ -97,11 +97,7 @@ class MultiManager(Generic[_E]):
             raise ValueError("Use '.clear_all_exps_result(security=True)' to clear all results.")
 
         if security and isinstance(security, bool):
-            for exp in qurry_progressbar(
-                self.exps.values(),
-                desc="Clear jobs result...",
-                bar_format="qurry-barless",
-            ):
+            for exp in self.exps.values():
                 exp.afterwards.clear_result(security=security, mute_warning=True)
             if not mute_warning:
                 warnings.warn(
@@ -394,10 +390,12 @@ class MultiManager(Generic[_E]):
             chunks_num = very_easy_chunk_size(
                 tasks_num=len(initial_config_list),
                 num_process=DEFAULT_POOL_SIZE,
-                max_chunk_size=DEFAULT_POOL_SIZE,
+                max_chunk_size=DEFAULT_POOL_SIZE * 2,
             )
 
-            pool = get_context("spawn").Pool(processes=DEFAULT_POOL_SIZE, maxtasksperchild=4)
+            pool = get_context("spawn").Pool(
+                processes=DEFAULT_POOL_SIZE, maxtasksperchild=chunks_num * 2
+            )
             with pool as p:
 
                 exps_iterable = qurry_progressbar(
@@ -409,18 +407,12 @@ class MultiManager(Generic[_E]):
                     total=len(initial_config_list),
                     desc="MultiManager building...",
                 )
-                exps_iterable.set_description_str(
-                    f"Loading {len(initial_config_list)} experiments..."
-                )
                 for new_exps, config in exps_iterable:
                     current_multimanager.register(
                         current_id=new_exps.commons.exp_id,
                         config=config,
                         exps_instance=new_exps,
                     )
-                exps_iterable.set_description_str(
-                    f"Loading {len(initial_config_list)} experiments done"
-                )
         else:
             exps_iterable = qurry_progressbar(
                 (
--- qurry/qurrium/multimanager/utils.py
+++ qurry/qurrium/multimanager/utils.py
@@ -66,11 +66,11 @@ def experiment_writer(
         )
 
         chunks_num, chunks_sorted_list, _distributions = very_easy_chunk_distribution(
-            respect_memory_array[1:], DEFAULT_POOL_SIZE, DEFAULT_POOL_SIZE
+            respect_memory_array[1:], DEFAULT_POOL_SIZE, DEFAULT_POOL_SIZE * 2
         )
 
         exporting_pool = get_context("spawn").Pool(
-            processes=DEFAULT_POOL_SIZE, maxtasksperchild=chunks_num
+            processes=DEFAULT_POOL_SIZE, maxtasksperchild=chunks_num * 2
         )
         with exporting_pool as ep:
             export_imap_result = qurry_progressbar(
