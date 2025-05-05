f301a735418304c8c9a2f5b73606081e701a9b3a
harui2019 <harui2019@proton.me>
2025-05-05 18:16:46 +0800

    ci(pytest): update

--- tests/process/test_shadow.py
+++ tests/process/test_shadow.py
@@ -8,7 +8,8 @@ import numpy as np
 
 from qurry.capsule import quickRead
 from qurry.qurrent.randomized_measure.utils import bitstring_mapping_getter
-from qurry.process.classical_shadow import classical_shadow_complex, classical_shadow_availability
+from qurry.process.classical_shadow import classical_shadow_complex, ClassicalShadowComplex
+from qurry.process.classical_shadow.matrix_calcution import JAX_AVAILABLE
 
 FILE_LOCATION = os.path.join(os.path.dirname(__file__), "shadow-case.json")
 
@@ -84,86 +85,53 @@ shadow_cases: list[ShadowCase] = [
 def test_shadow(shadow_case: ShadowCase):
     """Test the classical_shadow_complex function."""
 
-    assert classical_shadow_availability[1]["Rust"] == "No", (
-        "Rust is not available." + f" Check the error: {classical_shadow_availability[2]}"
-    )
-
     _bitstring_mapping, final_mapping = bitstring_mapping_getter(
         shadow_case["counts"], shadow_case["arguments"]["registers_mapping"]
     )
+    rho_methods = ["numpy", "numpy_precomputed", "numpy_flatten"] + (
+        ["jax_flatten"] if JAX_AVAILABLE else []
+    )
+    trace_methods = ["trace_of_matmul", "einsum_ij_ji", "einsum_aij_bji_to_ab_numpy"] + (
+        ["einsum_aij_bji_to_ab_jax"] if JAX_AVAILABLE else []
+    )
+    result: dict[str, ClassicalShadowComplex] = {}
 
     # Call the classical_shadow_complex function with the provided arguments
-    result_py = classical_shadow_complex(
-        shots=shadow_case["arguments"]["shots"],
-        counts=shadow_case["counts"],
-        random_unitary_um=shadow_case["random_unitary_ids"],
-        selected_classical_registers=[
-            final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
-        ],
-        backend="Python",
-    )
-    result_py_single_process = classical_shadow_complex(
-        shots=shadow_case["arguments"]["shots"],
-        counts=shadow_case["counts"],
-        random_unitary_um=shadow_case["random_unitary_ids"],
-        selected_classical_registers=[
-            final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
-        ],
-        backend="Python",
-        multiprocess=False,
-    )
-    result_py_einsum_ij_ij = classical_shadow_complex(
-        shots=shadow_case["arguments"]["shots"],
-        counts=shadow_case["counts"],
-        random_unitary_um=shadow_case["random_unitary_ids"],
-        selected_classical_registers=[
-            final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
-        ],
-        backend="Python",
-        method="hilbert_schmidt_inner_product",
-    )
-    result_py_single_process_einsum_ij_ij = classical_shadow_complex(
-        shots=shadow_case["arguments"]["shots"],
-        counts=shadow_case["counts"],
-        random_unitary_um=shadow_case["random_unitary_ids"],
-        selected_classical_registers=[
-            final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
-        ],
-        backend="Python",
-        multiprocess=False,
-        method="hilbert_schmidt_inner_product",
-    )
-    # result_rust = classical_shadow_complex(
-    #     shots=shadow_case["arguments"]["shots"],
-    #     counts=shadow_case["counts"],
-    #     random_unitary_um=shadow_case["random_unitary_ids"],
-    #     selected_classical_registers=[
-    #         final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
-    #     ],
-    #     backend="Rust",
-    # )
-    # result_rust_einsum_ij_ij = classical_shadow_complex(
-    #     shots=shadow_case["arguments"]["shots"],
-    #     counts=shadow_case["counts"],
-    #     random_unitary_um=shadow_case["random_unitary_ids"],
-    #     selected_classical_registers=[
-    #         final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
-    #     ],
-    #     backend="Rust",
-    #     method="hilbert_schmidt_inner_product",
-    # )
+    for rho_method in rho_methods:
+        for trace_method in trace_methods:
+            result[rho_method + "." + trace_method] = classical_shadow_complex(
+                shots=shadow_case["arguments"]["shots"],
+                counts=shadow_case["counts"],
+                random_unitary_um=shadow_case["random_unitary_ids"],
+                selected_classical_registers=[
+                    final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
+                ],
+                rho_method=rho_method,
+                trace_method=trace_method,
+            )
 
-    compare_list = [
-        (result_py, "result_py"),
-        (result_py_single_process, "result_py_single_process"),
-        (result_py_einsum_ij_ij, "result_py_einsum_ij_ij"),
-        (result_py_single_process_einsum_ij_ij, "result_py_single_process_einsum_ij_ij"),
-        # (result_rust, "result_rust"),
-        # (result_rust_einsum_ij_ij, "result_rust_einsum_ij_ij"),
-    ]
+    # result["rust"] = classical_shadow_complex(
+    #     shots=shadow_case["arguments"]["shots"],
+    #     counts=shadow_case["counts"],
+    #     random_unitary_um=shadow_case["random_unitary_ids"],
+    #     selected_classical_registers=[
+    #         final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
+    #     ],
+    #     backend="Rust",
+    # )
+    # result["rust_einsum_ij_ji"] = classical_shadow_complex(
+    #     shots=shadow_case["arguments"]["shots"],
+    #     counts=shadow_case["counts"],
+    #     random_unitary_um=shadow_case["random_unitary_ids"],
+    #     selected_classical_registers=[
+    #         final_mapping[qi] for qi in shadow_case["arguments"]["selected_qubits"]
+    #     ],
+    #     backend="Rust",
+    #     method="einsum_ij_ji",
+    # )
 
     # Compare the result with the expected answer
-    for result_tmp, name in compare_list:
+    for name, result_tmp in result.items():
         assert np.abs(result_tmp["purity"] - shadow_case["answer"]["purity"]) < 1e-12, (
             "The result is not correct,"
             + f"{name}: {result_tmp['purity']} != "
@@ -173,7 +141,7 @@ def test_shadow(shadow_case: ShadowCase):
             "The trace of the expect_rho should be 1: " + f"{np.trace(result_tmp['expect_rho'])}."
         )
 
-    for (result_tmp_1, name_1), (result_tmp_2, name_2) in combinations(compare_list, 2):
+    for (name_1, result_tmp_1), (name_2, result_tmp_2) in combinations(result.items(), 2):
         assert np.abs(result_tmp_1["purity"] - result_tmp_2["purity"]) < 1e-12, (
             "The result is not correct,"
             + f"{name_1}: {result_tmp_1['purity']} != {name_2}: {result_tmp_2['purity']}"
