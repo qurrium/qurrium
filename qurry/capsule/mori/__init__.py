bd7475eae9c9c39b69c5259c47541e68ad5b8ba7
harui2019 <harui2019@proton.me>
2025-05-25 04:42:06 +0800

    feat(capsule): delete deprecated DefaultConfig

--- qurry/capsule/mori/__init__.py
+++ qurry/capsule/mori/__init__.py
@@ -3,15 +3,14 @@
 ## Why Mori?
 
     There are many dedicated data structures for Qurrium
-    If we say one of them like a tree in forest, 
-    then all data structures combine, 
-    it makes a forest or '森' read as mori in Japanese. 
-    Definitely NOT because I'm a DeadBeat, 
-    the fan of Hololive VTuber Mori Calliope, 
+    If we say one of them like a tree in forest,
+    then all data structures combine,
+    it makes a forest or '森' read as mori in Japanese.
+    Definitely NOT because I'm a DeadBeat,
+    the fan of Hololive VTuber Mori Calliope,
     and I didn't want to name something after her for a not short time.
 
 """
 
 from .taglist import TagList, key_tuple_loads, tuple_str_parse
 from .gitsync import GitSyncControl
-from .config import DefaultConfig
