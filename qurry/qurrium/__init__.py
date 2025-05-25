971bf36cd85b0d5d780946a24af769447a1b88f3
harui2019 <harui2019@proton.me>
2025-05-25 15:48:23 +0800

    feat(qurrium): add import QurriumPrototype

--- qurry/qurrium/__init__.py
+++ qurry/qurrium/__init__.py
@@ -1,5 +1,6 @@
 """Qurrium - A Qiskit Macro (:mod:`qurry.qurrium`)"""
 
+from .qurrium import QurriumPrototype
 from .samplingqurry import SamplingExecuter
 from .wavesqurry import WavesExecuter
 from .utils import decomposer
