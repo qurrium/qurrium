bb880385108a710fb31f5e064b9a9820044d1343
harui2019 <harui2019@proton.me>
2025-06-10 19:58:22 +0800

    chore(qurrium): easier import path

--- qurry/qurrium/__init__.py
+++ qurry/qurrium/__init__.py
@@ -4,3 +4,5 @@ from .qurrium import QurriumPrototype
 from .samplingqurry import SamplingExecuter
 from .wavesqurry import WavesExecuter
 from .utils import decomposer
+from .experiment import ExperimentPrototype, Commonparams, ArgumentsPrototype, AnalysesContainer
+from .analysis import AnalysisPrototype
