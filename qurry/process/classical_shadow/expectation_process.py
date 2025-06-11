4fde0aeaaaece98ef297b09643a1f0f726803826
harui2019 <harui2019@proton.me>
2025-06-11 22:50:11 +0800

    feat(process.classical_shadow): add worst max norm upperbound

--- qurry/process/classical_shadow/expectation_process.py
+++ qurry/process/classical_shadow/expectation_process.py
@@ -3,6 +3,7 @@
 
 """
 
+from typing import Optional
 import warnings
 import numpy as np
 
@@ -12,6 +13,115 @@ from .matrix_calcution import (
     AllTraceRhoMethod,
 )
 from ..exceptions import AccuracyProbabilityCalculationError, AccuracyProbabilityWarning
+
+
+def dim_check(
+    op: np.ndarray[tuple[int, int], np.dtype[np.complex128]],
+) -> tuple[int, int]:
+    r"""Check the dimension of the operator.
+
+    The dimension of the operator is defined as follows,
+    .. math::
+        \text{dim}(X) = 2^n
+    where :math:`X` is the operator, and :math:`n` is the number of qubits.
+
+    Args:
+        op (np.ndarray[tuple[int, int], np.dtype[np.complex128]]):
+            The operator to be checked.
+
+    Returns:
+        tuple[int, int]: The dimension of the operator and the number of qubits.
+
+    Raises:
+        ValueError: If the shape of the operator is not a square matrix with size 2^n,
+        where n is the number of qubits.
+    """
+    n = int(np.log2(op.shape[0]))
+    dim = 2**n
+    if dim != op.shape[0]:
+        raise ValueError(
+            "The shape of the operator must be a square matrix with size 2^n, "
+            "where n is the number of qubits."
+        )
+    return dim, n
+
+
+def inverted_quantum_channel(
+    op: np.ndarray[tuple[int, int], np.dtype[np.complex128]],
+) -> np.ndarray[tuple[int, int], np.dtype[np.complex128]]:
+    r"""Inverted quantum channel.
+
+    The inverted quantum channel is defined as follows,
+    .. math::
+        \mathcal{M}_{n}^{-1}(X) = (2^n + 1)X - \mathbb{I}
+    where :math:`\mathcal{M}_{n}^{-1}` is the inverted quantum channel,
+    which mentioned in the paper before Algorithm 1.
+
+    where :math:`X` is the operator, and :math:`n` is the number of qubits.
+
+    Args:
+        op (np.ndarray[tuple[int, int], np.dtype[np.complex128]]):
+            The operator to be inverted.
+
+    Returns:
+        np.ndarray[tuple[int, int], np.dtype[np.complex128]]:
+            The inverted operator.
+
+    Raises:
+        ValueError: If the shape of the operator is not a square matrix with size 2^n,
+        where n is the number of qubits.
+    """
+    dim, _ = dim_check(op)
+    return (dim + 1) * op - np.eye(dim, dtype=np.complex128)  # type: ignore
+
+
+def traceless(
+    op: np.ndarray[tuple[int, int], np.dtype[np.complex128]],
+) -> np.ndarray[tuple[int, int], np.dtype[np.complex128]]:
+    r"""Make the operator traceless.
+
+    The traceless operator is defined as follows,
+    .. math::
+        \text{traceless}(O) = O - \frac{\text{tr}(O)}{2^n} \mathbb{I}
+    where :math:`O` is the operator, and :math:`n` is the number of qubits,
+    which mentioned in the supplementary material Lemma S1.
+
+    Args:
+        op (np.ndarray[tuple[int, int], np.dtype[np.complex128]]):
+            The operator to be made traceless.
+
+    Returns:
+        np.ndarray[tuple[int, int], np.dtype[np.complex128]]:
+            The traceless operator.
+    """
+    dim, _ = dim_check(op)
+    return op - (np.trace(op) / dim) * np.eye(dim, dtype=np.complex128)
+
+
+def largest_shadow_norm_squared_upperbound(
+    op: np.ndarray[tuple[int, int], np.dtype[np.complex128]],
+) -> float:
+    r"""Calculate the largest shadow norm upper bound.
+
+    The largest shadow norm upper bound is defined as follows,
+    .. math::
+        || O ||_{\text{shadow}}^2 \leq 4^n || O ||_{\infty}^2
+    where :math:`O` is the operator, and :math:`n` is the number of qubits,
+    which mentioned in the paper at Theorem 1 (informal version).
+
+    This is the worst scenario of the shadow norm
+    for its scaling can be reduced to :math:`3^n || O ||_{\infty}^2`,
+    which is the significantly lower bound than the worst case scenario.
+
+    Args:
+        op (np.ndarray[tuple[int, int], np.dtype[np.complex128]]):
+            The operator to be calculated.
+
+    Returns:
+        float: The largest shadow norm upper bound.
+    """
+    _dim, n = dim_check(op)
+    return (4**n) * (np.linalg.norm(op, ord=np.inf) ** 2)
 
 
 def accuracy_predict_epsilon_calc(
@@ -32,11 +142,8 @@ def accuracy_predict_epsilon_calc(
     and :math:`M` is the number of given operators,
     and :math:`N` is the number of classical snapshots.
     The :math:`|| O_i - \frac{\text{tr}(O_i)}{2^n} ||_{\text{shadow}}^2` is maximum shadow norm,
-    which is defined in the supplementary material with value between 0 and 1.
 
-    Due to maximum shadow norm is complex and it is a norm,
-    we suppose we have the worst case scenario,
-    where the maximum shadow norm is 1 as default.
+    Due to maximum shadow norm is complex, we suppose 1 for it should be under the order of 1.
     Thus, we can simplify the equation to:
     .. math::
         N = \frac{34}{\epsilon^2}
@@ -54,6 +161,67 @@ def accuracy_predict_epsilon_calc(
     """
 
     return np.sqrt(34 / num_classical_snapshot) * max_shadow_norm
+
+
+def worst_accuracy_predict_epsilon_calc(
+    num_classical_snapshot: int,
+    given_operators: list[np.ndarray[tuple[int, int], np.dtype[np.complex128]]],
+) -> tuple[float, float]:
+    r"""Calculate the prediction of accuracy in worst scenario, 
+    which used the notation :math:`\epsilon`
+    and mentioned in Theorem S1 in the supplementary material,
+    the equation (S13) in the supplementary material.
+
+    We can calculate the prediction of accuracy :math:`\epsilon` from the equation (S13)
+    in the supplementary material, the equation (S13) is as follows,
+    .. math::
+        N = \frac{34}{\epsilon^2} \max_{1 \leq i \leq M}
+        || O_i - \frac{\text{tr}(O_i)}{2^n} ||_{\text{shadow}}^2
+
+    where :math:`\epsilon` is the prediction of accuracy,
+    and :math:`M` is the number of given operators,
+    and :math:`N` is the number of classical snapshots.
+    The :math:`|| O_i - \frac{\text{tr}(O_i)}{2^n} ||_{\text{shadow}}^2` is maximum shadow norm,
+
+    And we also know the largest upper bound of the shadow norm is
+    .. math::
+        || O ||_{\text{shadow}}^2 \leq 4^n || O ||_{\infty}^2
+    where :math:`O` is the any operator, and :math:`n` is the number of qubits,
+
+    So we set the shadow norm as follows,
+    .. math::
+        \chi = || O_i - \frac{\text{tr}(O_i)}{2^n} ||_{\text{shadow}} \\
+        \chi_{\infty} = 4^n || O_i - \frac{\text{tr}(O_i)}{2^n} ||_{\infty}^2 \\
+        \chi^2 \leq \chi_{\infty}
+    and we can simplify the equation to:
+    .. math::
+        N = \frac{34}{\epsilon^2} \max_{1 \leq i \leq M} \chi^2 
+            \leq \frac{34}{\epsilon^2} \max_{1 \leq i \leq M} \chi_{\infty}^2
+    Then get:
+    .. math::
+        \epsilon \leq \sqrt{\frac{34}{N}} \max_{1 \leq i \leq M} \chi_\infty
+
+    Args:
+        num_classical_snapshot (int):
+            The number of classical snapshots.
+            It is :math:`N` in the equation.
+        given_operators (list[np.ndarray[tuple[int, int], np.dtype[np.complex128]]]):
+            The list of the operators to estimate.
+
+    Returns:
+        tuple[float, float]: 
+            The worst accuracy prediction epsilon and the worst maximum shadow norm.
+    """
+    if num_classical_snapshot <= 0:
+        raise ValueError("The number of classical snapshots must be greater than 0.")
+    if len(given_operators) == 0:
+        raise ValueError("The list of given operators must not be empty.")
+    squared_upperbound_list = [
+        largest_shadow_norm_squared_upperbound(traceless(op)) for op in given_operators
+    ]
+    max_inf_norms = np.sqrt(max(squared_upperbound_list))
+
+    return np.sqrt(34 / num_classical_snapshot) * max_inf_norms, max_inf_norms
 
 
 def accuracy_prob_comp_delta_calc(
@@ -184,7 +352,7 @@ def prediction_algorithm(
     classical_snapshots_rho: dict[int, np.ndarray[tuple[int, int], np.dtype[np.complex128]]],
     given_operators: list[np.ndarray[tuple[int, int], np.dtype[np.complex128]]],
     accuracy_prob_comp_delta: float = 0.01,
-    max_shadow_norm: float = 1.0,
+    max_shadow_norm: Optional[float] = None,
     trace_method: AllTraceRhoMethod = DEFAULT_ALL_TRACE_RHO_METHOD,
 ) -> tuple[list[np.ndarray[tuple[int, ...], np.dtype[np.complex128]]], float, int, float, float]:
     r"""Calculate the prediction of accuracy and the number of estimators.
@@ -199,7 +367,9 @@ def prediction_algorithm(
         accuracy_prob_comp_delta (float, optional):
             The accuracy probability component delta. Defaults to 0.01.
         max_shadow_norm (float, optional):
-            The maximum shadow norm. Defaults to 1.
+            The maximum shadow norm. Defaults to None.
+            If it is None, it will be calculated by the largest shadow norm upper bound.
+            If it is not None, it must be a positive float number.
             It is :math:`|| O_i - \frac{\text{tr}(O_i)}{2^n} ||_{\text{shadow}}^2` in equation.
         trace_method (AllTraceRhoMethod, optional):
             The method to calculate the trace for searching esitmator.
@@ -227,9 +397,14 @@ def prediction_algorithm(
             "the number of given operators must be greater than 0."
         )
 
-    accuracy_predict_epsilon = accuracy_predict_epsilon_calc(
-        num_classical_snapshot, max_shadow_norm
-    )
+    if max_shadow_norm is not None:
+        accuracy_predict_epsilon = accuracy_predict_epsilon_calc(
+            num_classical_snapshot, max_shadow_norm
+        )
+    else:
+        accuracy_predict_epsilon, max_shadow_norm = worst_accuracy_predict_epsilon_calc(
+            num_classical_snapshot, given_operators
+        )
     num_of_estimators, actual_accuracy_prob_comp_delta = num_of_esitmator_calc(
         num_classical_snapshot, num_of_given_operators, accuracy_prob_comp_delta
     )
