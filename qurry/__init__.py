46343a535f1201cf26ef6b794a74636b52d7ab89
harui2019 <harui2019@proton.me>
2025-06-12 21:56:29 +0800

    feat(boorust): add magnet_square

--- qurry/__init__.py
+++ qurry/__init__.py
@@ -11,7 +11,7 @@ from .qurrent import EntropyMeasure, ShadowUnveil
 # from .qurmagsq import MagnetSquare
 # from .qurstrop import StringOperator
 
-from .qurrium import WavesExecuter, SamplingExecuter
+from .qurries import WavesExecuter, SamplingExecuter, MagnetSquare, ZDirMagnetSquare, StringOperator
 
 from .tools import (
     BackendWrapper,
@@ -37,6 +37,7 @@ try:
     sys.modules["qurry.boorust.bit_slice"] = qurry.boorust.bit_slice  # type: ignore
     sys.modules["qurry.boorust.randomized"] = qurry.boorust.randomized  # type: ignore
     sys.modules["qurry.boorust.hadamard"] = qurry.boorust.hadamard  # type: ignore
+    sys.modules["qurry.boorust.magnet_square"] = qurry.boorust.magnet_square  # type: ignore
     sys.modules["qurry.boorust.dummy"] = qurry.boorust.dummy  # type: ignore
     sys.modules["qurry.boorust.test"] = qurry.boorust.test  # type: ignore
     # sys.modules["qurry.boorust.shadow"] = qurry.boorust.shadow  # type: ignore
--- qurry/qurries/__init__.py
+++ qurry/qurries/__init__.py
@@ -11,6 +11,7 @@
 from .samplingqurry import SamplingExecuter, SamplingExecuterMeasureArgs
 from .wavesqurry import WavesExecuter, WavesExecuterMeasureArgs
 from .magnet_square import MagnetSquare, MagnetSquareMeasureArgs
+from .magnet_square_z import ZDirMagnetSquare, ZDirMagnetSquareMeasureArgs
 from .string_operator import StringOperator, StringOperatorMeasureArgs
 
 # from .twisted_operator import TwistedOperator
