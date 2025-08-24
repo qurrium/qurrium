ccac9c39ae26df9e1b57d1184e26563d148ab485
harui2019 <harui2019@proton.me>
2025-08-25 03:23:53 +0800

    feat(process.classical_shadow): - introduce spreadout formally - merge rho_m_flatten

--- qurry/process/classical_shadow/container.py
+++ qurry/process/classical_shadow/container.py
@@ -22,6 +22,10 @@ class ClassicalShadowBasic(TypedDict):
     """The list of the selected_classical_registers."""
     taking_time: float
     """The time taken for the calculation."""
+    snapshots: int
+    """The number of random basis for classical shadow."""
+    shots: int
+    """The number of shots."""
 
 
 class ClassicalShadowMeanRho(ClassicalShadowBasic):
