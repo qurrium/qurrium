d7530e0194fee1d4178f3b5931c7b7edd489a712
harui2019 <harui2019@proton.me>
2025-09-09 19:13:07 +0800

    feat(process.classical_shadow.utils): more efficient multi_counts_to_basis_spin

--- qurry/process/classical_shadow/utils/basis_spin_fmt.py
+++ qurry/process/classical_shadow/utils/basis_spin_fmt.py
@@ -10,98 +10,80 @@ And export the format to a text file that can be read by
 `Predicting Properties of Quantum Many-Body Systems
 <https://github.com/hsinyuan-huang/predicting-quantum-properties>`_ .
 
-- Single shots basis-spin Output format (for PQP)
-
-.. code-block:: text
-
-    [system size]
-    [X/Y/Z for qubit 1] [-1/1 for qubit 1] [X/Y/Z for qubit 2] [-1/1 for qubit 2] ...
-    [X/Y/Z for qubit 1] [-1/1 for qubit 1] [X/Y/Z for qubit 2] [-1/1 for qubit 2] ...
-
 """
 
-from typing import Literal, Union
-
-from .spreadout import spreadout
-
-
-def validate_counts_and_basis(
-    idx: int,
-    single_counts: dict[str, int],
-    single_random_basis: dict[int, int],
-) -> str:
-    """Validate the single counts and random basis, then return bitstring.
-
-    Args:
-        idx (int): The index of the current counts and random basis
-        single_counts (dict[str, int]): The counts of single-shot results.
-        single_random_basis (dict[int, int]): Mapping of qubit indices to random basis.
-
-    Returns:
-        str: The validated bitstring.
-    """
-    bitstring = next(iter(single_counts.keys()))
-    if len(bitstring) != len(single_random_basis):
-        raise ValueError(
-            "The length of the bitstring must match the number of qubits in single_random_basis. "
-            + f"Bitstring and its length: '{bitstring}', {len(bitstring)}. "
-            + f"Random basis and its length: '{single_random_basis}', {len(single_random_basis)}. "
-            + f"Index: {idx}."
-        )
-    return bitstring
-
-
-def combine_counts_and_basis(
-    idx: int,
-    single_counts: dict[str, int],
-    single_random_basis: dict[int, int],
-) -> tuple[list[int], list[int]]:
-    """Single counts processing for PQP.
-
-    Args:
-        idx (int): The index of the current counts and random basis
-        single_counts (dict[str, int]): The counts of single-shot results.
-        single_random_basis (dict[int, int]): Mapping of qubit indices to random basis.
-
-    Returns:
-        tuple[list[int], list[int]]:
-            A tuple containing pauli basis and spin outcomes.
-    """
-
-    return list(single_random_basis.values()), [
-        int(bits) if bits == "1" else -1
-        for bits in reversed(validate_counts_and_basis(idx, single_counts, single_random_basis))
-    ]
+from typing import Literal, Union, Sequence
+from pathlib import Path
 
 
 def multi_counts_to_basis_spin(
     shots: int,
     counts: list[dict[str, int]],
-    random_basis: dict[int, dict[int, Union[Literal[0, 1, 2], int]]],
+    random_basis_array: list[list[Union[Literal[0, 1, 2], int]]],
 ) -> tuple[list[list[int]], list[list[int]]]:
-    """Convert a Qurrium experiment to Predicting Quantum Properties result.
+    """Convert counts to single shots basis-spin format.
 
     Args:
         shots (int):
             The number of shots.
         counts (list[dict[str, int]]):
             The list of the counts.
-        random_basis (dict[int, dict[int, Union[Literal[0, 1, 2], int]]]):
+        random_basis_array (list[list[Union[Literal[0, 1, 2], int]]]):
             The random basis for classical shadow.
 
     Returns:
         tuple[list[list[int]], list[list[int]]]:
             A tuple containing a list of pauli basis and a list of spin outcomes
     """
-    _shots, singleshot_counts, singleshot_random_basis = spreadout(shots, counts, random_basis)
+    pauli_basis, spin_outcome = [], []
+    for idx, (single_counts, single_random_basis) in enumerate(zip(counts, random_basis_array)):
+        partial_pauli_basis = [single_random_basis for _ in range(shots)]
+        partial_spin_outcome = []
 
-    results = [
-        combine_counts_and_basis(idx, single_counts, single_random_basis)
-        for (idx, single_random_basis), single_counts in zip(
-            singleshot_random_basis.items(), singleshot_counts
-        )
-    ]
+        for bitstring, count in single_counts.items():
+            spin_values = [1 if bits == "1" else -1 for bits in reversed(bitstring)]
+            partial_spin_outcome.extend([spin_values] * count)
 
-    pauli_basis, spin_outcome = map(list, zip(*results)) if results else ([], [])
+        if len(partial_spin_outcome) != shots:
+            raise ValueError(
+                "The single counts should only contain one bitstring with count equal to shots. "
+                + f"Counts: {single_counts}. Index: {idx}."
+            )
+        pauli_basis.extend(partial_pauli_basis)
+        spin_outcome.append(partial_spin_outcome)
 
     return pauli_basis, spin_outcome
+
+
+def measurements_export(
+    pauli_basis: Sequence[Sequence[int]],
+    spin_outcome: Sequence[Sequence[int]],
+    system_size: int,
+    filename: Union[str, Path],
+) -> Union[str, Path]:
+    """Export the measurement data.
+
+    Args:
+        pauli_basis (Sequence[Sequence[int]]):
+            The list of Pauli basis measurements. (X: 0, Y: 1, Z: 2)
+        spin_outcome (Sequence[Sequence[int]]):
+            The list of spin outcomes. (1, -1)
+        system_size (int):
+            The size of the quantum system, which is the number of qubits.
+        filename (Union[str, Path]):
+            The path to the output file.
+
+    Returns:
+        The path to the output file.
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(f"{system_size}\n")
+        for single_pauli, single_spin in zip(pauli_basis, spin_outcome):
+            f.writelines(
+                " ".join(
+                    (f"{chr(88 + pauli)} {spin}" for pauli, spin in zip(single_pauli, single_spin))
+                )
+            )
+
+    return filename
