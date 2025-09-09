6a5d3c681864f06565f3bf9044dda8af8ececbad
harui2019 <harui2019@proton.me>
2025-09-09 19:12:08 +0800

    fix(process.classical_shadow.trace_predict_process): reorder import

--- qurry/process/classical_shadow/trace_predict_process/__init__.py
+++ qurry/process/classical_shadow/trace_predict_process/__init__.py
@@ -5,17 +5,17 @@ This module is used to process the rho dictionary for classical shadow.
 
 """
 
-from .prediction import prediction_algorithm
-from .trace_mean_core import (
-    mean_rho_core,
-    trace_rho_square_core,
-    SingleTraceMethod,
-    TraceMethod,
-    ListTraceMethod,
-    DEFAULT_ALL_TRACE_RHO_METHOD,
-)
 from .matrix_calcution import (
     set_cpu_only,
+    SingleTraceMethod,
+    ListTraceMethod,
     BACKEND_AVAILABLE as classical_shadow_matrix_availability,
     JAX_AVAILABLE,
 )
+from .trace_mean_core import (
+    mean_rho_core,
+    trace_rho_square_core,
+    TraceMethod,
+    DEFAULT_ALL_TRACE_RHO_METHOD,
+)
+from .prediction import prediction_algorithm
