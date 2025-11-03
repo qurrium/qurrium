d07cee9ce1cd88bf1131aa65fd4f16180d2a3070
harui2019 <harui2019@proton.me>
2025-11-03 14:28:48 +0800

    feat(experiment): transfer classmethod quantites from Experiment to Analysis

--- qurry/qurrium/experiment/experiment.py
+++ qurry/qurrium/experiment/experiment.py
@@ -699,13 +699,6 @@ class ExperimentPrototype(ABC, Generic[_A, _R]):
         )
 
     # analysis
-    @classmethod
-    @abstractmethod
-    def quantities(cls) -> dict[str, Any]:
-        """Computing specific squantity.
-        Where should be overwritten by each construction of new measurement.
-        """
-
     @abstractmethod
     def analyze(self) -> _R:
         """Analyzing the example circuit results in specific method.
