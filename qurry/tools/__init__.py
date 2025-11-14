0cb5b3dbcafcba4cdf8a21ff8dd49431103b4db2
harui2019 <harui2019@proton.me>
2025-11-14 19:18:09 +0800

    refactor(qurrium.utils): reorganize

--- qurry/tools/__init__.py
+++ qurry/tools/__init__.py
@@ -13,6 +13,8 @@ from .parallelmanager import (
     workers_distribution,
     DEFAULT_POOL_SIZE,
     DEFAULT_START_METHOD,
+    very_easy_chunk_size,
+    very_easy_chunk_distribution
 )
 from .progressbar import qurry_progressbar, set_pbar_description
 from .datetime import current_time, DatetimeDict
