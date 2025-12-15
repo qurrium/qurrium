d0757a4d6723d7a308dffe6cd085e32ddf43ca16
harui2019 <harui2019@proton.me>
2025-12-15 14:51:53 +0800

    fix(experiment): naming

--- qurry/qurrium/multimanager/process.py
+++ qurry/qurrium/multimanager/process.py
@@ -3,9 +3,7 @@
 from typing import Any
 from pathlib import Path
 
-from .arguments import MultiCommonparams
 from .container import _E
-from ..utils import IOComplex
 from ..experiment import ExperimentPrototype, Export
 
 
@@ -102,40 +100,3 @@ def multiprocess_writer_wrapper(
         tuple[str, dict[str, str]]: The ID of experiment and the files of experiment.
     """
     return multiprocess_writer(*all_arguments)
-
-
-def datetimedict_process(
-    multicommons: MultiCommonparams,
-    naming_complex: IOComplex,
-    multiconfig_name_v5: Path,
-    multiconfig_name_v7: Path,
-    is_read_or_retrieve: bool,
-    read_from_tarfile: bool,
-    old_files: dict[str, Any],
-):
-    """Process the datetime dict of multimanager.
-
-    Args:
-        multicommons (MultiCommonparams): The common parameters of multimanager.
-        naming_complex (IOComplex): The complex of IO.
-        multiconfig_name_v5 (Path): The path of multiConfig in v5.
-        multiconfig_name_v7 (Path): The path of multiConfig in v7.
-        is_read_or_retrieve (bool): Whether read or retrieve.
-        read_from_tarfile (bool): Whether read from tarfile.
-        old_files (dict[str, Any]): The old files.
-    """
-
-    if "build" not in multicommons.datetimes and not is_read_or_retrieve:
-        multicommons.datetimes.add_only("build")
-
-    if naming_complex.tarLocation.exists():
-        if (not multiconfig_name_v5.exists()) and (not multiconfig_name_v7.exists()):
-            multicommons.datetimes.add_serial("decompress")
-        elif read_from_tarfile:
-            multicommons.datetimes.add_serial("decompressOverwrite")
-
-    # readV5 files re-export
-    if multiconfig_name_v5.exists():
-        multicommons.datetimes.add_only("readV7")
-        for k in old_files.keys():
-            multicommons.files.pop(k, None)
