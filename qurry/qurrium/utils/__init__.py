9d37c7ef0d7a34b302cce199534cbb7b16ba0ab4
harui2019 <harui2019@proton.me>
2025-12-15 14:37:31 +0800

    fix(utils): update func na,ing

--- qurry/qurrium/utils/__init__.py
+++ qurry/qurrium/utils/__init__.py
@@ -5,8 +5,8 @@ from .counts import get_counts_and_exceptions, bitstring_mapping_getter
 from .qasm import qasm_dumps, qasm_version_detect, qasm_loads, AvailableQASMVersions
 from .inputfixer import damerau_levenshtein_distance, outfields_check, outfields_hint
 from .iocontrol import (
-    naming,
-    IOComplex,
+    folder_naming,
+    ExportFolderNaming,
     FULL_SUFFIX_OF_COMPRESS_FORMAT,
     STAND_COMPRESS_FORMAT,
     RJUST_LEN,
