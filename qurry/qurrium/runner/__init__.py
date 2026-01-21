e37896044dba54e62b505e2a04d4cce848d51e6d
harui2019 <harui2019@proton.me>
2026-01-21 21:55:30 +0800

    feat: remove runner module

--- qurry/qurrium/runner/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Runner for running on Remote Backends (:mod:`qurry.qurrium.runner`)
-
-It is only for pendings and retrieve to remote backend like IBMQ, IBM, or some ThirdParty.
-
-"""
-
-from .runner import Runner, ThirdPartyRunner
-from .utils import retrieve_counter
