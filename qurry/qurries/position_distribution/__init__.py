c0e9916849ad0a2b35eb1d636a6be0d574f6556a
harui2019 <harui2019@proton.me>
2026-04-08 02:37:48 +0800

    fix(qurries): remove null proposal

--- qurry/qurries/position_distribution/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Position Distribution - PositionDistribution (:mod:`qurry.qurries.position_distribution`)
-
-- Proposal for a new Qurrium module
-- Not yet implemented
-
-"""
--- qurry/qurries/twisted_operator/__init__.py
+++
@@ -1,8 +0,0 @@
-"""TwistedOperator - Twisted Operator
-(:mod:`qurry.qurries.twisted_operator`)
-
-- Formerly known as `qurtwistop`
-- Proposal for a new Qurrium module
-- Not yet implemented
-
-"""
