0230fca35b96e68b31f1fb3bc6e5d382162d4990
harui2019 <harui2019@proton.me>
2026-07-31 14:37:57 +0800

    fix(process.classical_shadow): remove unpickle-able MappingProxyType

--- qurry/process/classical_shadow/rho_process/unitary_set.py
+++ qurry/process/classical_shadow/rho_process/unitary_set.py
@@ -5,7 +5,6 @@ The followings are unitary operators for our classical shadow implementation.
 """
 
 from typing import TypedDict
-from types import MappingProxyType as MPT
 from collections.abc import Sequence
 from dataclasses import dataclass
 import functools as ft
@@ -263,7 +262,7 @@ class ShadowRandomBasis:
     ]
     """The gate names and parameters for each basis. """
 
-    basis_projecters: MPT[tuple[int, str], npt.NDArray[np.complex128]]
+    basis_projecters: dict[tuple[int, str], npt.NDArray[np.complex128]]
     r"""The basis projectors for each basis and bitstring. 
     
     The basis projectors are defined as:
@@ -271,7 +270,7 @@ class ShadowRandomBasis:
     .. math::
         P_{mk}^{i} = U_{mi}^{\dagger} |b_k \rangle\langle b_k| U_{mi}
     """
-    basis_precomputed_rho_m_k_i: MPT[tuple[int, str], npt.NDArray[np.complex128]]
+    basis_precomputed_rho_m_k_i: dict[tuple[int, str], npt.NDArray[np.complex128]]
     r"""The precomputed :math:`\rho_{mk}^{i}` matrices for each basis and bitstring.
 
     The precomputed :math:`\rho_{mk}^{i}` matrices are defined as:
@@ -280,7 +279,7 @@ class ShadowRandomBasis:
         P_{mk}^{i} = U_{mi}^{\dagger} |b_k \rangle\langle b_k| U_{mi} \\
         \rho_{mk}^{i} = 3 P_{mk}^{i} - \mathbb{I}  
     """
-    pauli_projecters: MPT[tuple[int, str], npt.NDArray[np.complex128]]
+    pauli_projecters: dict[tuple[int, str], npt.NDArray[np.complex128]]
     r"""The Pauli projectors for each basis and bitstring.
     
     The Pauli projectors are defined as:
@@ -290,7 +289,7 @@ class ShadowRandomBasis:
 
     where :math:`\sigma_{i}` is the Pauli operator for the i-th qubit.
     """
-    pauli_precomputed_rho_m_k_i: MPT[tuple[int, str], npt.NDArray[np.complex128]]
+    pauli_precomputed_rho_m_k_i: dict[tuple[int, str], npt.NDArray[np.complex128]]
     r"""The precomputed :math:`\rho_{mk}^{i}` matrices for each Pauli basis and bitstring (tuple key).
 
     The precomputed :math:`\rho_{mk}^{i}` matrices are defined as:
@@ -311,28 +310,28 @@ class ShadowRandomBasis:
     `Unraveling the Mystery <https://pennylane.ai/demos/tutorial_diffable_shadows#unraveling-the-mystery>`_
     """
 
-    basis_projecters_2: MPT[int, npt.NDArray[np.complex128]]
+    basis_projecters_2: dict[int, npt.NDArray[np.complex128]]
     r"""The basis projectors for each basis and bitstring (int key).
     
     This is the same as :attr:`basis_projecters` but with a single integer key
     ``direction * 10 + ord(b_k) - 48`` instead of a tuple, for vectorized computation.
     """
 
-    basis_precomputed_rho_m_k_i_2: MPT[int, npt.NDArray[np.complex128]]
+    basis_precomputed_rho_m_k_i_2: dict[int, npt.NDArray[np.complex128]]
     r"""The precomputed :math:`\rho_{mk}^{i}` matrices for each Pauli basis and bitstring (int key).
 
     This is the same as :attr:`pauli_precomputed_rho_m_k_i` but with a single integer key
     ``direction * 10 + ord(b_k) - 48`` instead of a tuple, for vectorized computation.
     """
 
-    pauli_projecters_2: MPT[int, npt.NDArray[np.complex128]]
+    pauli_projecters_2: dict[int, npt.NDArray[np.complex128]]
     r"""The Pauli basis projectors for each Pauli basis and bitstring (int key).
 
     This is the same as :attr:`pauli_projecters` but with a single integer key
     ``direction * 10 + ord(b_k) - 48`` instead of a tuple, for vectorized computation.
     """
 
-    pauli_precomputed_rho_m_k_i_2: MPT[int, npt.NDArray[np.complex128]]
+    pauli_precomputed_rho_m_k_i_2: dict[int, npt.NDArray[np.complex128]]
     r"""The precomputed :math:`\rho_{mk}^{i}` matrices for each Pauli basis and bitstring (int key).
     
     This is the same as :attr:`pauli_precomputed_rho_m_k_i` but with a single integer key
@@ -441,24 +440,24 @@ class ShadowRandomBasis:
             for direction in [0, 1, 2]
             for b_k in ["0", "1"]
         }
-        quick_setter("basis_projecters", MPT(_basis_proj))
-        quick_setter("basis_projecters_2", MPT(_remapper(_basis_proj)))
+        quick_setter("basis_projecters", _basis_proj)
+        quick_setter("basis_projecters_2", _remapper(_basis_proj))
 
         _basis_rho_m_k_i = {k: (3 * v) - IDENTITY for k, v in _basis_proj.items()}
-        quick_setter("basis_precomputed_rho_m_k_i", MPT(_basis_rho_m_k_i))
-        quick_setter("basis_precomputed_rho_m_k_i_2", MPT(_remapper(_basis_rho_m_k_i)))
+        quick_setter("basis_precomputed_rho_m_k_i", _basis_rho_m_k_i)
+        quick_setter("basis_precomputed_rho_m_k_i_2", _remapper(_basis_rho_m_k_i))
 
         _pauli_proj = {
             (direction, b_k): ((1 / 2) * ((1 - 2 * int(b_k)) * PAULI[direction] + IDENTITY))
             for direction in [0, 1, 2]
             for b_k in ["0", "1"]
         }
-        quick_setter("pauli_projecters", MPT(_pauli_proj))
-        quick_setter("pauli_projecters_2", MPT(_remapper(_pauli_proj)))
+        quick_setter("pauli_projecters", _pauli_proj)
+        quick_setter("pauli_projecters_2", _remapper(_pauli_proj))
 
         _pauli_rho_m_k_i = {k: (3 * v) - IDENTITY for k, v in _pauli_proj.items()}
-        quick_setter("pauli_precomputed_rho_m_k_i", MPT(_pauli_rho_m_k_i))
-        quick_setter("pauli_precomputed_rho_m_k_i_2", MPT(_remapper(_pauli_rho_m_k_i)))
+        quick_setter("pauli_precomputed_rho_m_k_i", _pauli_rho_m_k_i)
+        quick_setter("pauli_precomputed_rho_m_k_i_2", _remapper(_pauli_rho_m_k_i))
 
     # I/O methods for exporting and ingesting ShadowRandomBasis data.
     def export(self) -> ShadowRandomBasisData:
